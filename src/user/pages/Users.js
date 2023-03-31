3f2be30b044f2909c2b1ff82c62254d6d4039d7b
Striker2569 <harsh.chauhan123@icloud.com>
2023-03-31 11:09:20 +0530

    Today I learnt how to style components

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,6 +1,17 @@
 import React from "react";
+import UserList from "../components/UserList";
+
 const Users = ()=>{
-    return <h2>User works</h2>
+    const USERS = [
+    {
+        id:'u1',
+        name:'Harsh Chauhan',
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0szgOQjoMtERPPGc94M59ryDj0IJgtUhlmALZEBY&s',
+        places: 3, 
+    }
+];
+return <UserList items= {USERS} />
+        {/* <h2>UserList not working</h2> */}
 }
 
 export default Users;
